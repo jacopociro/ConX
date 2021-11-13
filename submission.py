a9cc6a055f48fa62a78740e0627527f3200f0fd6
jacopociro <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>
2021-11-13 19:07:11 +0100

    commit

--- submission.py
+++ submission.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python
 
 def main():
-    print("Hello world")
+    i=0
+    if i < 5:
+        print("Hello world")
+        i = i + 1
+    else: 
+        print("hai rotto i coglioni")
+        
+
+
 
 if __name__ == '__main__':
     main()
